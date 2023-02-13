0eaca64a6256212a66ae8e0d5dd0a52b63812da6
wangcanjia <wangcanjia1@yixia.com>
2023-02-13 18:56:07 +0800

    BytesToString StringToBytes

--- string.go
+++ string.go
@@ -3,6 +3,7 @@ package labutils
 import (
 	"regexp"
 	"strings"
+	"unsafe"
 )
 
 // TrimHtml 过滤html代码
@@ -23,4 +24,42 @@ func TrimHtml(src string) string {
 	re, _ = regexp.Compile("\\s{2,}")
 	src = re.ReplaceAllString(src, "\n")
 	return strings.TrimSpace(src)
+}
+
+// StringToBytes
+// converts string to byte slice without a memory allocation.
+// 将字符串转换为字节切片
+func StringToBytes(s string) []byte {
+	if len(s) == 0 {
+		return []byte("")
+	}
+	return *(*[]byte)(unsafe.Pointer(
+		&struct {
+			string
+			Cap int
+		}{s, len(s)},
+	))
+}
+
+// BytesToString
+// converts byte slice to string without a memory allocation.
+// 将字节切片转换为字符串
+func BytesToString(b []byte) string {
+	if len(b) == 0 {
+		return ""
+	}
+	return *(*string)(unsafe.Pointer(&b))
+}
+
+// BytesSplit
+// split the byte slice by the specified size.
+// 按照指定长度分割字节切片
+func BytesSplit(buf []byte, size int) [][]byte {
+	var chunk []byte
+	chunks := make([][]byte, 0, len(buf)/size+1)
+	for len(buf) >= size {
+		chunk, buf = buf[:size], buf[size:]
+		chunks = append(chunks, chunk)
+	}
+	return chunks
 }
